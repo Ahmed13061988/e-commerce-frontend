aec5b53e23da5951255f49670402510bdd964b7a
Ahmed <wolktheline@yahoo.com>
2020-12-15 21:08:12 -0700

    almost done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,7 +9,8 @@ const Cart = (props) => {
     const cartItems = props.cartItems.map((cartItem) => <Item item={cartItem} handleButtonClick={() => props.handleButtonClick(cartItem)} buttonText={buttonText} />)
     // you will pass Item the callback fn to handle deleting an item. handleButtonClick = props.handleDeleteItem(cartItem).
     // over here you should also pass in button text. 
-
+    
+    // const total = 
     return (
         <div>
             <h4>Shopping Cart</h4>
